332a018a21360552c2e983d1ce91cbdc4f8f7791
glibin <glibin.v@gmail.com>
2022-04-22 23:29:02 +0300

    settings

--- lib/ueberauth/strategy/pipedrive/oauth.ex
+++ lib/ueberauth/strategy/pipedrive/oauth.ex
@@ -18,31 +18,18 @@ defmodule Ueberauth.Strategy.Pipedrive.OAuth do
   ]
 
   @doc """
-  Construct a client for requests to Pipedrive.
-  Optionally include any OAuth2 options here to be merged with the defaults:
-      Ueberauth.Strategy.Pipedrive.OAuth.client(
-        redirect_uri: "http://localhost:4000/auth/pipedrive/callback"
-      )
+  Construct a client for requests to Google.
+
   This will be setup automatically for you in `Ueberauth.Strategy.Pipedrive`.
-  These options are only useful for usage outside the normal callback phase of
-  Ueberauth.
+
+  These options are only useful for usage outside the normal callback phase of Ueberauth.
   """
   def client(opts \\ []) do
-    config =
-      :ueberauth
-      |> Application.fetch_env!(Ueberauth.Strategy.Pipedrive.OAuth)
-      |> check_credential(:client_id)
-      |> check_credential(:client_secret)
-
-    client_opts =
-      @defaults
-      |> Keyword.merge(config)
-      |> Keyword.merge(opts)
-
+    config = Application.get_env(:ueberauth, __MODULE__, [])
+    opts = @defaults |> Keyword.merge(opts) |> Keyword.merge(config) |> resolve_values()
     json_library = Ueberauth.json_library()
 
-    client_opts
-    |> OAuth2.Client.new()
+    OAuth2.Client.new(opts)
     |> OAuth2.Client.put_serializer("application/json", json_library)
   end
 
@@ -84,33 +71,12 @@ defmodule Ueberauth.Strategy.Pipedrive.OAuth do
     |> OAuth2.Strategy.AuthCode.get_token(params, headers)
   end
 
-  defp check_credential(config, key) do
-    check_config_key_exists(config, key)
-
-    case Keyword.get(config, key) do
-      value when is_binary(value) ->
-        config
-
-      {:system, env_key} ->
-        case System.get_env(env_key) do
-          nil ->
-            raise "#{inspect(env_key)} missing from environment, expected in config :ueberauth, Ueberauth.Strategy.Pipedrive"
-
-          value ->
-            Keyword.put(config, key, value)
-        end
+  defp resolve_values(list) do
+    for {key, value} <- list do
+      {key, resolve_value(value)}
     end
   end
 
-  defp check_config_key_exists(config, key) when is_list(config) do
-    unless Keyword.has_key?(config, key) do
-      raise "#{inspect(key)} missing from config :ueberauth, Ueberauth.Strategy.Pipedrive"
-    end
-
-    config
-  end
-
-  defp check_config_key_exists(_, _) do
-    raise "Config :ueberauth, Ueberauth.Strategy.Pipedrive is not a keyword list, as expected"
-  end
+  defp resolve_value({m, f, a}) when is_atom(m) and is_atom(f), do: apply(m, f, a)
+  defp resolve_value(v), do: v
 end
